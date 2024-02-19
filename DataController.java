6afec678c9fb0001c2f39a272b4958e7a171cc92
Abhishek singh <145861092+singhabhishek6363@users.noreply.github.com>
2024-02-19 15:14:48 +0530

    Add files via upload

---
+++ DataController.java
@@ -0,0 +1,17 @@
+package com.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class DataController {
+	@PostMapping("/per")
+	public String CalculatePercentage(@RequestParam String name, Double marks1,Double marks2,Double marks3,Double marks4) {
+		  Double total = (marks1 + marks2 + marks3 + marks4)/4;
+        return "Hey "+name+" ,your percentage is "+total+"%";
+	}
+	
+
+}
